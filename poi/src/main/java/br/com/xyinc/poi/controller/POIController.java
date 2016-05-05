69989c06a34ab549265b4d7d665e90aa400754a8
alissonmbr <alissonmbr@gmail.com>
2016-05-04 23:16:12 -0300

    Poi model, creating a simple Rest service

--- poi/src/main/java/br/com/xyinc/poi/controller/POIController.java
+++ poi/src/main/java/br/com/xyinc/poi/controller/POIController.java
@@ -3,13 +3,17 @@ package br.com.xyinc.poi.controller;
 /**
  * Created by alisson on 5/4/16.
  */
-import org.springframework.web.bind.annotation.RestController;
+import br.com.xyinc.poi.model.Poi;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class POIController {
+
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public Poi index(@RequestParam(value="name") String name, @RequestParam(value="x") int x, @RequestParam(value="y") int y) {
+        return new Poi(name, x, y);
     }
+
 }
---
+++ poi/src/main/java/br/com/xyinc/poi/model/Poi.java
@@ -0,0 +1,70 @@
+package br.com.xyinc.poi.model;
+
+/**
+ * Created by alisson on 5/4/16.
+ */
+public class Poi {
+    private String name;
+    private int x;
+    private int y;
+
+    public Poi(String name, int x, int y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Poi{" +
+                "name='" + name + '\'' +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Poi poi = (Poi) o;
+
+        if (x != poi.x) return false;
+        if (y != poi.y) return false;
+        return name.equals(poi.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + x;
+        result = 31 * result + y;
+        return result;
+    }
+}
