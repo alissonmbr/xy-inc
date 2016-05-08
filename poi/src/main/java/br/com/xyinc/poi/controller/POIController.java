aac507e0936f3222fbaaab0bb3fe8c3be7b80aba
alissonmbr <alissonmbr@gmail.com>
2016-05-08 01:54:42 -0300

    Implementing services

--- poi/src/main/java/br/com/xyinc/poi/controller/POIController.java
+++ poi/src/main/java/br/com/xyinc/poi/controller/POIController.java
@@ -3,17 +3,34 @@ package br.com.xyinc.poi.controller;
 /**
  * Created by alisson on 5/4/16.
  */
+import br.com.xyinc.poi.dao.PoiDAO;
 import br.com.xyinc.poi.model.Poi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class POIController {
 
-    @RequestMapping("/")
-    public Poi index(@RequestParam(value="name") String name, @RequestParam(value="x") int x, @RequestParam(value="y") int y) {
-        return new Poi(name, x, y);
+    @Autowired
+    private PoiDAO poiDAO;
+
+    @RequestMapping("/add")
+    public Poi addPoi(@RequestParam(value="name") String name, @RequestParam(value="x") int x, @RequestParam(value="y") int y) {
+        return poiDAO.addPoi(new Poi(name, x, y));
+    }
+
+    @RequestMapping("/all")
+    public List<Poi> listAll() {
+        return poiDAO.findAll();
+    }
+
+    @RequestMapping("/reference")
+    public List<Poi> listByReference(@RequestParam(value="x") int x, @RequestParam(value="y") int y, @RequestParam(value="d") int d) {
+        return poiDAO.findByReference(x, y, d);
     }
 
 }
